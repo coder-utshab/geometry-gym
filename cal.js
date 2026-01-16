101ef2862076ff709af1c3569b0d4dd682129acc
coder-utshab <ghoshutshob470@gmail.com>
2026-01-17 01:19:17 +0530

    jv

--- cal.js
+++ cal.js
@@ -1,7 +1,27 @@
-
+/**
+ * objective: get  base, height of a triangle . calculate the are by  using the provided 
+ * formula. and then display they are.
+ * 
+ * step-1 get base value of the triangle,
+ * step-2 added an id in the base input field
+ * step-3 uses getelement by id to access the input field
+ * step-4 get value the input field.(value is string now)
+ * step-5 convert the value  to a number. use parseFloat
+ */
 
 function calculateTriangleArea(){
+    //get triangle base value
     const triangleBaseInput = document.getElementById('triangle-base');
     const triangleBaseText = triangleBaseInput.value;
-    console.log(triangleBaseText)
+    const base = parseFloat(triangleBaseInput)
+    console.log(base)
+
+    // console.log(base)
+
+    //get triangle height value
+const triangleHeightInput = document.getElementById('triagle-height');
+// const triangleHeightText = triangleHeightInput.value;
+const triangleHeightText = triangleHeightInput.value;
+const height = parseFloat(triangleHeightText)
+console.log(height);
 }
