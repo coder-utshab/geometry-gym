136fc1e1c56cf82b8767ef7f732aaebda47df445
coder-utshab <ghoshutshob470@gmail.com>
2026-01-17 13:54:27 +0530

    length

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,16 @@
 function calculateRectangleArea(){
-    console.log('calculate rect area');
+    // console.log('calculate rect area');
+    // get length of the rectangle
+
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+
+    // get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
 }
