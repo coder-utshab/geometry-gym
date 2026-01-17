471ce5f8dc397dedfb2ca79f67671d15269c4f5c
coder-utshab <ghoshutshob470@gmail.com>
2026-01-17 18:52:33 +0530

    bt

---
+++ scripts/parallelogram.js
@@ -0,0 +1,19 @@
+function calculateParallelogramArea() {
+    // const baseInput = document.getElementById('parallelogram-base');
+    // const baseText = baseInput.value;
+    // const base = parseFloat(baseText);
+    // console.log(base);
+
+
+    const  base = calculateParallelogramArea();
+    console.log(base);
+
+
+
+ }
+ function  getParallelogramBase(){
+ const baseInput = document.getElementById('parallelogram-base');
+    const baseText = baseInput.value;
+    const base = parseFloat(baseText);
+    return base;
+ }
